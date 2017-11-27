b6150d2fdad152e7acf7450dad24c492bd79a095
Ioannis Lazaridis <ounos@live.com>
2017-11-27 19:02:54 +0100

    i3 polybar ranger termite

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+# Script from github repo
+# Basic script to kill all old bars and launch new.
+
+# Terminate already running bad instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+# while grep -x polybar >/dev/null; do sleep 1; done
+
+polybar example
+
