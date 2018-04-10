68d520dae417d674086264ee4f57cf94553376ae
Ioannis Lazaridis <ounos@live.com>
2018-04-10 19:41:10 +0200

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,7 +5,6 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-# while grep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar example -r &
-
